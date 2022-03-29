2eb824853fc58132a2a92211f269242ab072fb3a
Krish Patel <99328839+krishxps@users.noreply.github.com>
2022-03-29 20:50:07 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberOfTime =  document.querySelectorAll(".drum").length;
+for(var i=0; i<numberOfTime ;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+       this.style.color = "red"
+    });
+}
+function firstButton(){
+    
+}
+  
